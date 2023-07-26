9505facc36362b3ce1d113dd25359607126cbe8d
JagsP23 <pullapuramjr@gmail.com>
2023-07-26 15:39:01 -0400

    changed the defualth path

--- src/main/java/com/example/demo/controller/HelloWorld.java
+++ src/main/java/com/example/demo/controller/HelloWorld.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
-    @GetMapping("/hello")
+    @GetMapping("/helloo")
     public String helloWorld(){
         return "Hello there";
     }
